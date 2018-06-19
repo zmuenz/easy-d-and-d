ea860bfa841ebc9bd54fb461378bd1d7d7e36b31
Zack Muenz <33330169+zmuenz@users.noreply.github.com>
2018-06-18 21:07:27 -0500

    Add files via upload

---
+++ classRoll.js
@@ -0,0 +1,3 @@
+const classes = ["barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "wizard"];
+
+var charClass = classes[Math.floor(Math.random() * classes.length)];
---
+++ raceRoll.js
@@ -0,0 +1,3 @@
+const races = ["dwarf", "elf", "gnome", "half-elf", "half-orc", "halfling", "human"];
+
+var charRace = races[Math.floor(Math.random() * races.length)];
