f6a810e976fa50b2b4d682fe229f08e330762671
Maldrek <scott.salem5@gmail.com>
2018-07-04 13:36:53 -0500

    fixed issue with package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Login } from "./pages/login";
-import { SignUp } from "./pages/signUp";
 import { CharacterPage } from "./pages/CharacterPage";
 import { NewCharacter } from "./pages/NewCharacter";
 import { Dashboard } from "./pages/Dashboard";
@@ -17,8 +15,6 @@ const App = () => (
       <NavBar />
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/login" component={Login} />
         <Route exact path="/characterpage" component={CharacterPage} />
         <Route exact path="/newcharacter" component={NewCharacter} />
         <Route exact path="/dashboard" component={Dashboard} />
