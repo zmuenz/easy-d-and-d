3159baa54411ed4f66a1ec21bc21eca917e07785
Maldrek <scott.salem5@gmail.com>
2018-07-07 14:04:20 -0500

    updated responsive nature and some styling on landing page

--- client/src/components/landingComponents/Parallax1/Parallax1.js
+++ client/src/components/landingComponents/Parallax1/Parallax1.js
@@ -7,25 +7,25 @@ const Parallax1 = () => {
             <div className="parallax"><img id='dice' src="http://paizo.com/image/content/CarrionCrown/PZO9046-BestiaryOpener.jpeg" alt='dnd d20 over character sheet' /></div>
             <h1 className='text-white' style={{marginTop: '30px'}}>A way out of the darkness!</h1>
             <div id="landingCards" style={{ display: 'flex', alignItems: 'space-around', flexFlow: 'row wrap' }}>
-                <div id='card1' className="card col-sm-3 col-md-3 text-center text-light grow">
+                <div id='card1' className="card col-4 col-sm-3 col-md-3 text-center text-light grow">
                     <i className='large material-icons card-img-top'>brightness_5</i>
                     <div className="card-body">
-                        <h5 className="card-title">A Bright Light</h5>
-                        <p className="card-text">Get out of the dungeon of paperwork and begin gaming in minutes!</p>
+                        <h5 className="card-title d-none d-block">A Shining Beacon of Hope</h5>
+                        <p className="card-text d-none d-md-block">Get out of the dungeon of paperwork and begin gaming in minutes!</p>
                     </div>
                 </div>
-                <div id='card2' className="card col-sm-3 col-md-3 text-center text-light grow">
+                <div id='card2' className="card col-4 col-sm-3 col-md-3 text-center text-light grow">
                 <i className='large material-icons card-img-top'>invert_colors</i>
                     <div className="card-body">
-                        <h5 className="card-title">A Tall Glass of Water</h5>
-                        <p className="card-text">Finally, everything you have been waiting for in easy character creation!</p>
+                        <h5 className="card-title d-none d-block">A Tall Glass of Water</h5>
+                        <p className="card-text d-none d-md-block">Finally, everything you have been waiting for in easy character creation!</p>
                     </div>
                 </div>
-                <div id='card3' className="card col-sm-3 col-md-3 text-center text-light grow">
+                <div id='card3' className="card col-4 col-sm-3 col-md-3 text-center text-light grow">
                 <i className='large material-icons card-img-top'>ac_unit</i>
                     <div className="card-body">
-                        <h5 className="card-title">A Cool Breeze in Summer</h5>
-                        <p className="card-text">Take your knowledge of DnD and put it to use quickly and efficiently!</p>
+                        <h5 className="card-title d-none d-block">A Cool Breeze in Summer</h5>
+                        <p className="card-text d-none d-md-block">Take your knowledge of DnD and put it to use quickly and efficiently!</p>
                     </div>
                 </div>
             </div>
