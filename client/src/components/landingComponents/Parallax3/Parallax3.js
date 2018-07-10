c1e4142e647c31d1fe87e7653e0cacc0ba09996c
Rachel Punches <rpunches@gmail.com>
2018-07-09 20:36:54 -0500

    random push

--- client/src/components/landingComponents/Parallax3/Parallax3.js
+++ client/src/components/landingComponents/Parallax3/Parallax3.js
@@ -7,7 +7,6 @@ import {
 } from 'reactstrap';
 import API from '../../../utils/API';
 
-<<<<<<< Updated upstream
 const Parallax3 = () => {
     return (
         <div id='parallax3' style={{ height: '500px' }}>
@@ -15,25 +14,9 @@ const Parallax3 = () => {
             <div id="landingCards" style={{ display: 'flex', alignItems: 'space-around', flexFlow: 'row wrap' }}>
                 <div id='card1' className="card col-sm-12 col-md-3 grow">
 
-=======
-class Parallax3 extends Component { 
-    render() {
-        return (
-            <div id='parallax3' style={{ height: '500px' }}>
-                <div className="parallax"></div>
-                <h1 className='text-white text-center' style={{ marginTop: '30px' }}></h1>
-                    <div id="landingCards" style={{ display: 'flex', alignItems: 'space-around', flexFlow: 'row wrap' }}>
-                    <div id='card1' className="card col-sm-12 col-md-3 grow">
->>>>>>> Stashed changes
                     <div className="card-body">
                         <Card>
-                            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                            <CardBody>
-                                <CardTitle>Character Name</CardTitle>
-                                <CardSubtitle>Card subtitle</CardSubtitle>
-                                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                                <Button>Profile</Button> <Button>Delete Character</Button>
-                            </CardBody>
+
                         </Card>
                     </div>
                 </div>
@@ -66,8 +49,8 @@ class Parallax3 extends Component {
                 </div>
             </div>
         </div>
-        );
-    };
-}
+    );
+};
+
 
 export default Parallax3;
