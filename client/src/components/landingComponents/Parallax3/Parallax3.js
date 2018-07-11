426085d9905a71cec9cfbeb2036ca533c37ed645
Rachel Punches <rpunches@gmail.com>
2018-07-10 20:09:45 -0500

    some char card changes to add so i can pull

--- client/src/components/landingComponents/Parallax3/Parallax3.js
+++ client/src/components/landingComponents/Parallax3/Parallax3.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
-import {
-    Card, CardImg, CardTitle, CardText,
-    CardSubtitle, CardBody, Button
-} from 'reactstrap';
+import CharCard from './landingComponents';
 import API from '../../../utils/API';
 
 const Parallax3 = () => {
@@ -15,23 +12,13 @@ const Parallax3 = () => {
                 <div id='card1' className="card col-sm-12 col-md-3 grow">
 
                     <div className="card-body">
-                        <Card>
-
-                        </Card>
+                        <CharCard></CharCard>                       
                     </div>
                 </div>
                 <div id='card2' className="card col-sm-12 col-md-3 grow">
 
                     <div className="card-body">
-                        <Card>
-                            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                            <CardBody>
-                                <CardTitle>Character Name</CardTitle>
-                                <CardSubtitle>Card subtitle</CardSubtitle>
-                                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                                <Button>Profile</Button> <Button>Delete Character</Button>
-                            </CardBody>
-                        </Card>
+                        <CharCard></CharCard>                      
                     </div>
                 </div>
                 <div id='card3' className="card col-sm-12 col-md-3 grow">
