a6c0b732d708fccf593ccb3dd43c51221dba4124
Kelsey Gasser <kjgasser@gmail.com>
2018-07-14 22:22:02 -0500

    logout route

--- client/src/components/landingComponents/Logout/Logout.js
+++ client/src/components/landingComponents/Logout/Logout.js
@@ -6,7 +6,7 @@ class Logout extends Component {
     
     handleFormSubmit = event => {
     sessionStorage.clear()
-    .then(this.props.history.push("/landing"));
+    .then(this.props.history.push("/"));
     
     }
 
