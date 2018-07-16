27c2272140178c2e282aff290dd7964f1d36809c
Zack Muenz <zackmuenz@gmail.com>
2018-07-16 16:32:47 -0500

    Fixed button link on dashboard

--- client/src/components/characterComponents/AddCharacter/AddCharacter.js
+++ client/src/components/characterComponents/AddCharacter/AddCharacter.js
@@ -9,22 +9,25 @@ import API from '../../../utils/API';
 import AddButton from '../AddButton/AddButton';
 
 class AddCharacter extends Component {
-    render() { 
-        return(
-<div id='card1' className="card col-sm-12 col-md-3 grow">
-<div className="card-body">
-    <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        <CardBody>
-            <CardTitle>Add a character?</CardTitle>
-            <CardSubtitle>Something awesome</CardSubtitle>
-            <CardText>Could be in store</CardText>
-            <AddButton>Duh I want to add!</AddButton> 
-        </CardBody>
-    </Card>
-</div>
-</div>
-    )}
-}
+    render() {
+        return (
+            <div id='card1' className="card col-sm-12 col-md-3 grow">
+                <a className="noLinkStyling" href='/newcharacter'>
+                    <div className="card-body">
+                        <Card>
+                            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle>Add a character?</CardTitle>
+                                <CardSubtitle>Something awesome</CardSubtitle>
+                                <CardText>Could be in store</CardText>
+                                <AddButton>Duh I want to add!</AddButton>
+                            </CardBody>
+                        </Card>
+                    </div>
+                </a>
+            </div>
+        )
+    }
+};
 
 export default AddCharacter;
